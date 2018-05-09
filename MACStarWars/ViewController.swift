1f57b7cec51a34844b575f329b6bb82fa4725853
Digvijay9 <digvijay.mallegowda_chinnappa@students.tamuk.edu>
2018-05-09 03:03:11 -0400

    Initial Commit

---
+++ MACStarWars/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MACStarWars
+//
+//  Created by digvijay mallegowda on 5/9/18.
+//  Copyright © 2018 digvijay mallegowda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
